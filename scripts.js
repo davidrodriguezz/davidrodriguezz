be96c37f8008215c2c66e835df18a0c155d8b5f5
davidrodriguezz <davidr13@ucm.es>
2023-08-14 22:36:03 +0200

    Abilities

--- scripts.js
+++ scripts.js
@@ -1,31 +1,48 @@
-document.getElementById('openNavImg').addEventListener('click', function() {
-    document.getElementById('openNavImg').style.transform = "scaleX(-1)";
-    document.getElementById('sideNav').style.width = '250px';
-    document.getElementById('openNavImg').style.left = '250px';
+document.addEventListener('DOMContentLoaded', function() {
+  const contentElement = document.getElementById('content');
+  contentElement.classList.add('fade');
 
+  const paragraphs = document.querySelectorAll('.fade-in-paragraph');
+  let animationDelay = 0.5; // Retraso inicial en segundos
+
+  paragraphs.forEach(paragraph => {
+    paragraph.style.transitionDelay = animationDelay + 's';
+    animationDelay += 0.5; // Incrementa el retraso para cada párrafo
   });
-  
+
+  window.addEventListener('scroll', function() {
+    paragraphs.forEach(paragraph => {
+      const rect = paragraph.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      if (rect.top < windowHeight) {
+        paragraph.classList.add('fade-in');
+      }
+    });
+  });
+
+  const openNavImg = document.getElementById('openNavImg');
+  const sideNav = document.getElementById('sideNav');
+
+  openNavImg.addEventListener('click', function() {
+    openNavImg.style.transform = "scaleX(-1)";
+    sideNav.style.width = '250px';
+    openNavImg.style.left = '250px';
+  });
+
   document.getElementById('closeNav').addEventListener('click', function() {
-    document.getElementById('openNavImg').style.transform = "scaleX(1)";
-    document.getElementById('sideNav').style.width = '0';
-    document.getElementById('openNavImg').style.left = '0';
+    openNavImg.style.transform = "scaleX(1)";
+    sideNav.style.width = '0';
+    openNavImg.style.left = '0';
+  });
 
+  // Agregar desplazamiento suave al enlace de Habilidades
+  document.getElementById('skillsLink').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const targetSection = document.getElementById('greenSection');
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
   });
-  document.addEventListener('scroll', function() {
-    const graySection = document.querySelector('.gray-section');
-    
-    const scrollTop = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const halfWindowHeight = windowHeight / 2;
-    
-    if (scrollTop >= halfWindowHeight) {
-      graySection.style.display = 'block';
-    } else {
-      graySection.style.display = 'none';
-    }
-  });
-  
-  
-  
-  
-  
+});
